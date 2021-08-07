dc665f8616d25cff7b4a2551a2325d5955b3c505
hamidreza Ranjbarpour <hamidrezarrj@gmail.com>
2021-08-07 10:33:06 +0430

    overriding  passport's default token lifetimes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
@@ -19,12 +20,15 @@ class AuthServiceProvider extends ServiceProvider
 
     /**
      * Register any authentication / authorization services.
-     *
+     * 
      * @return void
      */
     public function boot()
     {
+        
         $this->registerPolicies();
         Passport::routes();
+        Passport::tokensExpireIn(Carbon::now()->add('hours', 1));
+        
     }
 }
